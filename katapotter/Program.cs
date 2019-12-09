686edf12115b9a37a80f6f6665d03db69066f286
BTX <thomas.bourthoumieux@viacesi.fr>
2019-12-09 22:04:04 +0100

    fin testBank + structure katapotter

--- katapotter/Program.cs
+++ katapotter/Program.cs
@@ -10,10 +10,10 @@ namespace katapotter
 
         public static void Main()
         {
-            BankAccount ba = new BankAccount("Mr. Bryan Walton", 11.99);
+            /*BankAccount ba = new BankAccount("Mr. Bryan Walton", 11.99);
 
             ba.Debit(11.22);
-            Console.WriteLine("il vous reste actuellement {0} € sur votre compte en banque.", ba.Balance);
+            Console.WriteLine("il vous reste actuellement {0} € sur votre compte en banque.", ba.Balance);*/
         }
     }
 }
